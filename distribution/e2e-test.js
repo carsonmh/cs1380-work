b4e54fc6562542d62dc80c361c9a8a6787ef6718
Carson Harrell <thecarsonharrell@gmail.com>
2025-04-16 19:15:15 +0000

    nodes can connect to each other

--- distribution/e2e-test.js
+++ distribution/e2e-test.js
@@ -1,25 +1,65 @@
-const distribution = require('../config')
+const readline = require('readline');
+const distribution = require('../config');
 const id = distribution.util.id;
 
 const {mapper, reducer} = require('./engine/crawler-functions');
 const {indexReducer, indexMapper} = require('./engine/indexer')
 
 const crawlGroupGroup = {};
+let localServer = null;
 
-let localServer = null
+function promptForWorkerNodes() {
+  return new Promise((resolve) => {
+    const rl = readline.createInterface({
+      input: process.stdin,
+      output: process.stdout
+    });
 
-// const kafkaNode = {ip: '127.0.0.1', port: 9001, kafka: true}
-const workerNode1 = {ip: '127.0.0.1', port: 7000}
-const workerNode2 = {ip: '127.0.0.1', port: 7001}
-const workerNode3 = {ip: '127.0.0.1', port: 7002}
-const workerNode4 = {ip: '127.0.0.1', port: 7003}
+    const workerNodes = [];
+    
+    function askForNode() {
+      rl.question('Enter worker node IP and port (format: "ip:port"), or press enter to finish: ', (answer) => {
+        if (answer.trim() === '') {
+          rl.close();
+          resolve(workerNodes);
+        } else {
+          const [ip, port] = answer.split(':');
+          if (ip && port) {
+            workerNodes.push({ ip: ip.trim(), port: parseInt(port.trim()) });
+            askForNode();
+          } else {
+            console.log('Invalid format. Please use "ip:port" format.');
+            askForNode();
+          }
+        }
+      });
+    }
 
+    askForNode();
+  });
+}
 
-function start (){
-    const urls = [
+async function start() {
+  console.log("starting...")
+  const workerNodes = await promptForWorkerNodes();
+  
+  if (workerNodes.length === 0) {
+    console.log('No worker nodes provided. Exiting.');
+    process.exit(1);
+  }
+
+  // Populate crawlGroupGroup with the provided worker nodes
+  workerNodes.forEach(node => {
+    crawlGroupGroup[id.getID(node)] = node;
+  });
+
+  const crawlGroupConfig = {gid: 'workers'};
+  distribution.local.groups.put(crawlGroupConfig, crawlGroupGroup, (e, v) => {
+    distribution.workers.groups.put(crawlGroupConfig, crawlGroupGroup, (e, v) => {
+      const urls = [
         'https://law.justia.com/codes/alabama/2024/',
-      ]
-
+      ];
+    
       distribution.local.groups.get('workers', (e, group) => {
         const nodeToUrls = {}
         for(const url of urls) {
@@ -28,7 +68,7 @@ function start (){
           const node = group[nodeKey]
           if(nodeToUrls[nodeKey]) {
             nodeToUrls[nodeKey].push(url)
-          }else {
+          } else {
             nodeToUrls[nodeKey] = [url]
           }
         }
@@ -47,11 +87,13 @@ function start (){
           })
         }
       })
+    });
+  });
 }
 
 let iterations = 0;
 
-function run (cb) {
+function run(cb) {
   distribution.workers.mr.exec({keys: ['urls'], map: mapper, reduce: reducer}, (e, v) => {
     distribution.workers.mem.get(null, (e, v) => {
       let count = 0;
@@ -60,62 +102,49 @@ function run (cb) {
       }
 
       const serializedReducer = global.distribution.util.serialize(indexReducer);
-
       const updatedSerializedReducer = serializedReducer.replace('numDocs = 0;', `numDocs = ${count};`);
-
       let reducertfidf = global.distribution.util.deserialize(updatedSerializedReducer);
+      
       distribution.workers.mr.exec({keys: ['indexer'], map: indexMapper, reduce: reducertfidf}, (e, v) => {
         if(iterations < 1) {
           iterations += 1
           run(cb)
-        }else {
-          const remote = {service: 'status', method: 'stop'};
-          remote.node = workerNode2;
-          distribution.local.comm.send([], remote, (e, v) => {
-            remote.node = workerNode1;
-            distribution.local.comm.send([], remote, (e, v) => {
-              remote.node = workerNode3;
+        } else {
+          // Stop all worker nodes
+          const stopPromises = Object.values(crawlGroupGroup).map(node => {
+            return new Promise((resolve) => {
+              const remote = {service: 'status', method: 'stop', node};
               distribution.local.comm.send([], remote, (e, v) => {
-                remote.node = workerNode4;
-                distribution.local.comm.send([], remote, (e, v) => {
-                  localServer.close();
-                  cb(null, null)
-                })
-              })
-            })
-          })
+                resolve();
+              });
+            });
+          });
+
+          Promise.all(stopPromises).then(() => {
+            localServer.close();
+            cb(null, null);
+          });
         }
       })
     })
   })
 }
 
-crawlGroupGroup[id.getID(workerNode1)] = workerNode1
-crawlGroupGroup[id.getID(workerNode2)] = workerNode2
-crawlGroupGroup[id.getID(workerNode3)] = workerNode3
-crawlGroupGroup[id.getID(workerNode4)] = workerNode4
+// const startNodes = (cb) => {
+//   const spawnPromises = Object.values(crawlGroupGroup).map(node => {
+//     return new Promise((resolve) => {
+//       distribution.local.status.spawn(node, (e, v) => {
+//         resolve();
+//       });
+//     });
+//   });
 
-const startNodes = (cb) => {
-distribution.local.status.spawn(workerNode1, (e, v) => {
-    distribution.local.status.spawn(workerNode2, (e, v) => {
-      distribution.local.status.spawn(workerNode3, (e, v) => {
-        distribution.local.status.spawn(workerNode4, (e, v) => {
-          cb();
-        });
-     });
-    });
-  });
-};
+//   Promise.all(spawnPromises).then(() => {
+//     cb();
+//   });
+// };
 
 distribution.node.start((server) => {
-localServer = server;
-
-startNodes(() => {
-    const crawlGroupConfig = {gid: 'workers'};
-    distribution.local.groups.put(crawlGroupConfig, crawlGroupGroup, (e, v) => {
-      distribution.workers.groups.put(crawlGroupConfig, crawlGroupGroup, (e, v) => {
-        start();
-        });
-      });
-    });
+  localServer = server;
+  start();
 });
